c1418d33f861ae08943c8e5885256b21eccffb1c
Sean Kahler <itsbattledash@gmail.com>
2024-01-20 12:56:17 -0700

    Fix

--- maxima-lib/src/core/background_service_nix.rs
+++ maxima-lib/src/core/background_service_nix.rs
@@ -1,9 +1,9 @@
 use anyhow::{bail, Result};
+use base64::{engine::general_purpose, Engine};
 use dll_syringe::{process::OwnedProcess, Syringe};
 use log::debug;
 use reqwest::StatusCode;
 use serde::{Deserialize, Serialize};
-use base64::{engine::general_purpose, Engine};
 
 use is_elevated::is_elevated;
 
@@ -17,10 +17,18 @@ pub struct InjectArgs {
 
 pub async fn request_library_injection(pid: u32, path: &str) -> Result<()> {
     debug!("Injecting {}", path);
-    
-    let launch_args = InjectArgs { pid, path };
-    let b64 = general_purpose::STANDARD.encode(serde_json::to_string(&launch_args).unwrap());
 
-    run_wine_command("wine", module_path().parent().join("wine-injector.exe"), Some(vec![b64]))?;
+    let launch_args = InjectArgs {
+        pid,
+        path: path.to_owned(),
+    };
+
+    let b64 = general_purpose::STANDARD.encode(serde_json::to_string(&launch_args).unwrap());
+    run_wine_command(
+        "wine",
+        module_path().parent().join("wine-injector.exe"),
+        Some(vec![b64]),
+    )?;
+
     Ok(())
 }
