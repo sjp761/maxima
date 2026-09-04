3beb51ab43a762a7fc67b6e4192804839c2e84a1
sjp761 <171447425+sjp761@users.noreply.github.com>
2026-09-03 19:18:03 -0500

    Remove unused dependencies

--- maxima-lib/src/content/manager.rs
+++ maxima-lib/src/content/manager.rs
@@ -308,7 +308,7 @@ impl GameDownloader {
             return Ok(());
         }
 
-        handle_touchup_request(output_dir, None, &slug).await?;
+        handle_touchup_request(output_dir, wine_prefix_path, &slug).await?;
 
         info!("Installation finished!");
         completed_bytes.fetch_add(1, Ordering::SeqCst);
